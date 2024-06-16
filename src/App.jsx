8f04c1012b9358e2cecb1ea1ffc171e23fbd690f
VixoPlank <vichevasquez@gmail.com>
2024-06-15 22:27:06 -0400

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { getUserByName } from './api/userGitHub';
+import Header from './components/Header';
+import Search from './components/Search';
+import UserProfile from './components/UserProfile';
+import Spinner from './components/Spinner';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
+  const fetchUser = async (username) => {
+    setLoading(true);
+    setError(null);
+    setUser(null);
+
+    try {
+      const userData = await new Promise((resolve) =>
+        setTimeout(async () => {
+          resolve(await getUserByName(username));
+        }, 1000)
+      );
+
+      if (userData) {
+        setUser(userData);
+        setError(null);
+      } else {
+        setError('Usuario no encontrado :(');
+      }
+    } catch (error) {
+      setError('Error fetching user data');
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <Header />
+      <Search onSearch={fetchUser} />
+      {error && (
+        <p className='text-3xl lg:text-7xl text-gray-50 text-center'>{error}</p>
+      )}
+      {loading && <Spinner />}
+      <UserProfile user={user} />
+    </main>
+  );
 }
 
-export default App
+export default App;
